16646c6d91a97a588ce65813e36190510b231224
Thomas Vanegas <thomas.vanegas@mercadolibre.com.co>
2026-08-18 11:42:58 -0500

    feat: addProduct functionality implemeted. The function doesnt mutate the array

---
+++ addProduct.js
@@ -0,0 +1,18 @@
+const addProductToCatalog = (catalog, product) => {
+
+     const {name, price, stock} = product;
+
+    // En JS null, undefined, 0, false, NaN, "" (cadena vacía) son valores falsy.
+    if (!product || !product.name || !product.price || !product.stock) {
+        throw new Error("Product must have name, price, and stock properties.");
+    }
+
+    const existingProduct = catalog.find(p => p.name === name);
+    if (existingProduct) {
+        throw new Error(`Product with name "${name}" already exists in the catalog.`);
+    }
+
+    return [...catalog, product];
+};
+
+module.exports = addProductToCatalog;
--- main.js
+++ main.js
@@ -1,6 +1,30 @@
+const addProductToCatalog = require('./addProduct');
+
 const productsCatalog = [
     {name: "Mouse", price: 100, stock: 10},
     {name: "Keyboard", price: 200, stock: 5},
     {name: "Monitor", price: 500, stock: 2},
-]
+];
 
+const newProduct = {
+    name: "Webcam",
+    price: 250,
+    stock: 6,
+}
+
+const invalidProduct = {
+    name: "Mouse",
+    price: 150,
+    stock: 2
+}
+
+const updatedCatalog = addProductToCatalog(productsCatalog, newProduct);
+
+console.log("Original catalog:", productsCatalog);
+console.log("Updated catalog:", updatedCatalog);
+
+try {
+    const updatedCatalogWithInvalidProduct = addProductToCatalog(productsCatalog, invalidProduct);
+} catch (error) {
+    console.error("Error adding invalid product:", error.message);
+}
